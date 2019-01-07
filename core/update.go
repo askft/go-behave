9e01e47b0e6dbd1d0dc05e2bb6030b267a57fcb3
Alexander Skafte <alexanderskafte@protonmail.com>
2019-01-07 11:49:27 +0100

    Minor stylistic changes to Update

--- core/update.go
+++ core/update.go
@@ -3,14 +3,17 @@ package core
 // Update updates a node by calling its Enter method if it is not running,
 // then its Tick method, and finally Leave if it is not still running.
 func Update(node Node, ctx *Context) Status {
-	status := node.GetStatus()
-	if status != StatusRunning {
+
+	if node.GetStatus() != StatusRunning {
 		node.Enter(ctx)
 	}
-	status = node.Tick(ctx)
+
+	status := node.Tick(ctx)
 	node.SetStatus(status)
+
 	if status != StatusRunning {
 		node.Leave(ctx)
 	}
+
 	return status
 }
