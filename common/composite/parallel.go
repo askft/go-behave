30b47c09b5aaf0ef14e3a142ddb1a5380a492fb0
Alexander Skafte <alexanderskafte@protonmail.com>
2019-01-07 11:50:21 +0100

    Modify ParallelSequence and change its name to Parallel

---
+++ common/composite/parallel.go
@@ -0,0 +1,76 @@
+package composite
+
+import (
+	"github.com/askft/go-behave/core"
+)
+
+// Parallel updates all its children in parallel, i.e. every frame.
+// It does not retry on nodes that have failed or succeeded.
+//
+// succ/failReq is the minimum amount of nodes required to
+// succeed/fail for the parallel sequence node itself to succeed/fail.
+// A value of 0 for either node means that all nodes must succeed/fail.
+func Parallel(succReq, failReq int, children ...core.Node) core.Node {
+	base := core.NewComposite("Parallel", children)
+	if succReq == 0 {
+		succReq = len(children)
+	}
+	if failReq == 0 {
+		failReq = len(children)
+	}
+	return &parallel{
+		base,
+		succReq,
+		failReq,
+		0,
+		0,
+		make([]bool, len(children)),
+	}
+}
+
+type parallel struct {
+	*core.Composite
+	succReq   int
+	failReq   int
+	succ      int
+	fail      int
+	completed []bool
+}
+
+func (s *parallel) Enter(ctx *core.Context) {
+	s.succ = 0
+	s.fail = 0
+}
+
+func (s *parallel) Tick(ctx *core.Context) core.Status {
+
+	// Update every child that has not completed yet every tick.
+	for i := 0; i < len(s.Children); i++ {
+
+		// Ignore a child if has already succeeded or failed.
+		if s.completed[i] {
+			continue
+		}
+
+		// Update a child and count whether it succeeded or failed,
+		// and mark it as completed in either of those two cases.
+		switch core.Update(s.Children[i], ctx) {
+		case core.StatusSuccess:
+			s.succ++
+			s.completed[i] = true
+		case core.StatusFailure:
+			s.fail++
+			s.completed[i] = true
+		}
+	}
+
+	if s.succ >= s.succReq {
+		return core.StatusSuccess
+	}
+	if s.succ >= s.failReq {
+		return core.StatusFailure
+	}
+	return core.StatusRunning
+}
+
+func (s *parallel) Leave(ctx *core.Context) {}
