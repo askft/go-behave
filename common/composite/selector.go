80ccdc25f15d68001c45c72902cece2b66af097f
Alexander Skafte <alexanderskafte@protonmail.com>
2019-01-07 11:51:22 +0100

    Stylistic changes to Selector, and add doc comment

--- common/composite/selector.go
+++ common/composite/selector.go
@@ -4,35 +4,31 @@ import (
 	"github.com/askft/go-behave/core"
 )
 
-// Selector creates a new selector node.
+// Selector updates each child in order, returning success as soon as
+// a child succeeds. If a child returns Running, the selector node
+// will resume execution from that child the next tick.
 func Selector(children ...core.Node) core.Node {
 	base := core.NewComposite("Selector", children)
 	return &selector{Composite: base}
 }
 
-// selector ...
 type selector struct {
 	*core.Composite
 }
 
-// Enter ...
 func (s *selector) Enter(ctx *core.Context) {
 	s.Composite.CurrentChild = 0
 }
 
-// Tick ...
 func (s *selector) Tick(ctx *core.Context) core.Status {
-	for {
+	for s.CurrentChild < len(s.Children) {
 		status := core.Update(s.Children[s.CurrentChild], ctx)
 		if status != core.StatusFailure {
 			return status
 		}
 		s.Composite.CurrentChild++
-		if s.CurrentChild >= len(s.Children) {
-			return core.StatusFailure
-		}
 	}
+	return core.StatusFailure
 }
 
-// Leave ...
 func (s *selector) Leave(ctx *core.Context) {}
