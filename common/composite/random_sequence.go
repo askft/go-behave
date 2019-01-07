bbe067d07e057ad76a603985977f1c852f5d75ef
Alexander Skafte <alexanderskafte@protonmail.com>
2019-01-07 11:47:28 +0100

    Stylistic changes to RandomSequence, and add doc comment

--- common/composite/random_sequence.go
+++ common/composite/random_sequence.go
@@ -6,37 +6,32 @@ import (
 	"github.com/askft/go-behave/core"
 )
 
-// RandomSequence creates a new random sequence node.
+// RandomSequence works just like Sequence, except it shuffles
+// the order of its children every time it is re-updated.
 func RandomSequence(children ...core.Node) core.Node {
 	base := core.NewComposite("RandomSequence", children)
 	return &randomSequence{Composite: base}
 }
 
-// Enter ...
-func (s *randomSequence) Enter(ctx *core.Context) {
-	shuffle(s.Children)
-}
-
-// randomSequence ...
 type randomSequence struct {
 	*core.Composite
 }
 
-// Tick ...
+func (s *randomSequence) Enter(ctx *core.Context) {
+	shuffle(s.Children)
+}
+
 func (s *randomSequence) Tick(ctx *core.Context) core.Status {
-	for {
+	for s.CurrentChild < len(s.Children) {
 		status := core.Update(s.Children[s.CurrentChild], ctx)
 		if status != core.StatusSuccess {
 			return status
 		}
 		s.Composite.CurrentChild++
-		if s.CurrentChild >= len(s.Children) {
-			return core.StatusSuccess
-		}
 	}
+	return core.StatusSuccess
 }
 
-// Leave ...
 func (s *randomSequence) Leave(ctx *core.Context) {
 	s.Composite.CurrentChild = 0
 }
